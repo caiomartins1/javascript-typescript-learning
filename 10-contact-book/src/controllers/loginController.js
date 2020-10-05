7cdcb95ef3f03527d80d87389e4e6ed2b0bb411f
Caio Martins <dev.caiomartins@gmail.com>
2020-10-05 04:13:20 +0100

    user login

--- 10-contact-book/src/controllers/loginController.js
+++ 10-contact-book/src/controllers/loginController.js
@@ -1,8 +1,9 @@
 const Login = require('../models/LoginModel');
 
 exports.index = (req, res) => {
-  res.render('login');
-  return;
+  if (req.session.user) return res.render('logged');
+
+  return res.render('login');
 };
 
 exports.register = async (req, res) => {
@@ -26,4 +27,33 @@ exports.register = async (req, res) => {
     console.log(e);
     return res.render('404');
   }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const login = new Login(req.body);
+    await login.login();
+
+    if (login.errors.length > 0) {
+      req.flash('errors', login.errors);
+      req.session.save(function () {
+        return res.redirect('/login');
+      });
+      return;
+    }
+
+    req.flash('success', 'Logged In');
+    req.session.user = login.user;
+    req.session.save(function () {
+      return res.redirect('/login');
+    });
+  } catch (e) {
+    console.log(e);
+    return res.render('404');
+  }
+};
+
+exports.logout = (req, res) => {
+  req.session.destroy();
+  return res.redirect('/');
 };
--- 10-contact-book/src/models/LoginModel.js
+++ 10-contact-book/src/models/LoginModel.js
@@ -28,6 +28,26 @@ class Login {
     }
   }
 
+  async login() {
+    this.validate();
+    if (this.errors.length > 0) return;
+
+    this.user = await LoginModel.findOne({
+      email: this.body.email,
+    });
+
+    if (!this.user) {
+      this.errors.push('Invalid username or password');
+      return;
+    }
+
+    if (!bcryptjs.compareSync(this.body.password, this.user.password)) {
+      this.errors.push('Invalid username or password');
+      this.user = null;
+      return;
+    }
+  }
+
   async register() {
     this.validate();
 
@@ -44,19 +64,15 @@ class Login {
     const salt = bcryptjs.genSaltSync();
     this.body.password = bcryptjs.hashSync(this.body.password, salt);
 
-    try {
-      this.user = await LoginModel.create(this.body);
-    } catch (e) {
-      console.log(e);
-    }
+    this.user = await LoginModel.create(this.body);
   }
 
   async userExists() {
-    const user = await LoginModel.findOne({
+    this.user = await LoginModel.findOne({
       email: this.body.email,
     });
 
-    if (user) {
+    if (this.user) {
       this.errors.push('User already exists');
     }
   }
