3c0b6a6b11cf83562b03427578cf4dd657dcb71f
caiomartins1 <dev.caiomartins@gmail.com>
2021-01-19 16:40:09 +0000

    Creating seeds

---
+++ 12-API-REST/src/database/seeds/20210119162558-create-users.js
@@ -0,0 +1,33 @@
+const bcryptjs = require('bcryptjs');
+
+module.exports = {
+  up: async (queryInterface) => queryInterface.bulkInsert(
+    'users',
+    [
+      {
+        name: 'John Doe',
+        email: 'john@doe.com',
+        password_hash: await bcryptjs.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Jane Doe',
+        email: 'jane@doe.com',
+        password_hash: await bcryptjs.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'James Doe',
+        email: 'james@doe.com',
+        password_hash: await bcryptjs.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ],
+    {},
+  ),
+
+  down: async () => { },
+};
