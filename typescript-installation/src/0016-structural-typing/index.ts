5245e99dd21a32a2e047c9c2bbfd9b442020a4b7
Caio Martins <dev.caiomartins@gmail.com>
2021-06-17 00:55:27 +0100

    Added structural typing

---
+++ typescript-installation/src/0016-structural-typing/index.ts
@@ -0,0 +1,25 @@
+type User = {
+  username: string;
+  password: string;
+};
+
+type VerifyUserFn = (user: User, sentValue: User) => boolean;
+
+const verifyUser: VerifyUserFn = (user, sentValue) => {
+  return (
+    user.username === sentValue.username && user.password === sentValue.password
+  );
+};
+
+// TS wont look for the type User, but will look for the properties required for the method
+const bdUser = {
+  username: 'John',
+  password: 'test',
+};
+const sentUser = {
+  username: 'John',
+  password: 'test',
+  example: 'asdkhaskdjhasd',
+};
+
+const loggedIn = verifyUser(bdUser, sentUser);
